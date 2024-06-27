424de85356e2e38ae67e96dc0f2251c3f38814c6
unknown <gonzalezleonardoantonio056@gmail.com>
2024-06-27 17:38:42 -0300

    contact world

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,15 +1,21 @@
-import Script from 'next/script';
+//import Script from 'next/script';
+import world from '@/images/assets/world.mp4'
 
 export default function Contact() {
   return (
-    <main>
-      <section>
+    <main className='h-auto bg-[#010001]'>
+      <section >
 
 
-        <div>
-        <iframe src="https://lottie.host/embed/784b15fc-3032-4011-a681-4e1a3f0cf5e6/0rrGbGT9Al.json"></iframe>
+        <div className='h-[150px] flex justify-center'>
+          <video autoPlay muted loop className="w-[150px] h-[150px]">
+            <source src={world} type="video/mp4" />
+          </video>
         </div>
 
+        
+        {/*<iframe src="https://lottie.host/embed/784b15fc-3032-4011-a681-4e1a3f0cf5e6/0rrGbGT9Al.json"></iframe>*/}
+        
 
       </section>
     </main>
