f053883a0d02d07aefb9bd27319dfc56c817dc55
unknown <gonzalezleonardoantonio056@gmail.com>
2024-06-28 21:28:52 -0300

    pricess web

--- src/app/components/Precios.jsx
+++ src/app/components/Precios.jsx
@@ -8,7 +8,7 @@ export default function Precios(){
     <p className="mx-8 lg:mx-[48px] text-[18px] mt-3">Los planes para emprendedores son aplicables a vendedores de productos físicos o digitales, como revendedores, tiendas de indumentaria o comerciantes de productos de marca.</p>
     <p className="mx-8 lg:mx-[48px] text-[15px] mt-1 font-semibold">La cantidad de productos puede afectar significativamente el abonar de la página o el mantenimiento. </p>
     <p className="mx-8 lg:mx-[48px] text-[15px] mt-1 font-semibold">Hasta 20 productos y 4 secciones estos son los precios base:</p>
-    <section className="flex flex-wrap justify-center"></section>
+    
         
         <section className=" grid justify-center 
         md:flex md:flex-wrap md:justify-evenly md:my-[80px] my-[40px]">
@@ -18,7 +18,7 @@ export default function Precios(){
              <p className="pricing-plan">EMPRENDEDOR BÁSICO</p>
              <div className="price-value" data-currency="$ USD" data-currency-simple="USD">
              <p className="price-number">
-             $<span className="price-integer">30.000</span>
+             $<span className="price-integer">40.000</span>
              </p>
              </div>
              <div className="pricing-note">Mantenimiento: $12.000/m</div>
@@ -209,7 +209,7 @@ export default function Precios(){
              <p className="pricing-plan">EMPRENDEDOR ESTÁNDAR</p>
              <div className="price-value" data-currency="$ USD" data-currency-simple="USD">
              <p className="price-number">
-             $<span className="price-integer">45.000</span>
+             $<span className="price-integer">60.000</span>
              </p>
              </div>
              <div className="pricing-note">Mantenimiento: $18.000/m</div>
@@ -276,7 +276,7 @@ export default function Precios(){
                 />
               </g>
             </svg>
-            Dominio personalizado
+            Configuración de dominio
           </li>
           <li className="check-list-item">
           <svg
@@ -638,26 +638,31 @@ export default function Precios(){
       </div>  
         </section>
         
+
+        <h2 className="mx-8 lg:mx-[48px] text-[18px] font-bold text-[#977c4b] opacity-[50%] mt-[60px]"> - Planes para profesionales -</h2>
+        <p className="mx-8 lg:mx-[48px] text-[18px] mt-3">Los planes para profesionales son apropiados para aquellos que procuran su presencia online y/o quieren facilitar algún aspecto de su negocio mediante implementaciones de automatización o marketing.</p>
+
+
         <section className=" grid justify-center 
         md:flex md:flex-wrap md:justify-evenly md:my-[80px] my-[40px]">
              
-     {/*Estándar */}
+     {/*BÁSICO */}
              <div 
              style={{
                 background: 'linear-gradient(to right, #ffffff, #afafaf)',
                 
                 }
-                } className="card my-[50px] md:my-0">
+                } className="card mb-[30px] md:my-0">
 
              <div className="pricing-block-content">
              <p className="pricing-plan">PROFESINAL BÁSICO</p>
              <div className="price-value" data-currency="$ USD" data-currency-simple="USD">
              <p className="price-number">
-             $<span className="price-integer">55.000</span>
+             $<span className="price-integer">50.000</span>
              </p>
              
              </div>
-             <div className="pricing-note">Mantenimiento $10.000</div>
+             <div className="pricing-note">Mantenimiento $0</div>
              <ul className="check-list" role="list">
              <li className="check-list-item">
              <svg
@@ -689,7 +694,7 @@ export default function Precios(){
                 />
               </g>
             </svg>
-            Dominio personalizado
+            Busqueda y configuración de dominio
           </li>
           <li className="check-list-item">
           <svg
@@ -870,7 +875,7 @@ export default function Precios(){
              </p>
              <div className="price-value" data-currency="$ USD" data-currency-simple="USD">
              <p className="price-number">
-             $<span className="price-integer">70.000</span>
+             $<span className="price-integer">90.000</span>
              
              </p>
              
@@ -940,7 +945,7 @@ export default function Precios(){
                 />
               </g>
             </svg>
-            Optimización de SEO recurrente
+            Optimización de SEO
           </li>
           <li className="check-list-item">
           <svg
@@ -1069,6 +1074,38 @@ export default function Precios(){
               </g>
             </svg>
             Hasta 5 secciones
+          </li>
+          <li className="check-list-item">
+          <svg
+              version="1.0"
+              preserveAspectRatio="xMidYMid meet"
+              height="16"
+              viewBox="0 0 30 30.000001"
+              zoomAndPan="magnify"
+              width="16"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              xmlns="http://www.w3.org/2000/svg"
+              style={{ color: '000000' }}
+            >
+              <defs>
+                <clipPath id="id1">
+                  <path
+                    fill="#000000"
+                    clipRule="nonzero"
+                    d="M 2.328125 4.222656 L 27.734375 4.222656 L 27.734375 24.542969 L 2.328125 24.542969 Z M 2.328125 4.222656"
+                  />
+                </clipPath>
+              </defs>
+              <g clipPath="url(#id1)">
+                <path
+                  fillRule="nonzero"
+                  fillOpacity="1"
+                  d="M 27.5 7.53125 L 24.464844 4.542969 C 24.15625 4.238281 23.65625 4.238281 23.347656 4.542969 L 11.035156 16.667969 L 6.824219 12.523438 C 6.527344 12.230469 6 12.230469 5.703125 12.523438 L 2.640625 15.539062 C 2.332031 15.84375 2.332031 16.335938 2.640625 16.640625 L 10.445312 24.324219 C 10.59375 24.472656 10.796875 24.554688 11.007812 24.554688 C 11.214844 24.554688 11.417969 24.472656 11.566406 24.324219 L 27.5 8.632812 C 27.648438 8.488281 27.734375 8.289062 27.734375 8.082031 C 27.734375 7.875 27.648438 7.679688 27.5 7.53125 Z M 27.5 7.53125"
+                  fill="#000000"
+                />
+              </g>
+            </svg>
+            Sistemas de automatización
           </li>
 
         </ul>
