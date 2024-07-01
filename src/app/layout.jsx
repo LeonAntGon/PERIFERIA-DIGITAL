92e6b26a842435589199e3d604d2a50d4198b44b
unknown <gonzalezleonardoantonio056@gmail.com>
2024-07-01 17:07:46 -0300

    seo meta tags

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,18 +7,16 @@ const robo = Roboto({ subsets: ["latin"], weight: '400' });
 export const metadata = {
   title: "Periferia Digital",
   description: "Una perspectiva digital para escalar tu negocio!",
-  metadata: {
-    meta: [
-      { property: 'og:site_name', content: 'Periferia digital' },
-      { property: 'og:title', content: 'Periferia Digital' },
-      { property: 'og:description', content: 'Una perspectiva digital para escalar tu negocio!' },
-      { property: 'og:image', itemprop: 'image', content: 'http://pollosweb.wesped.es/programa_pollos/play.png' },
-      {property: "og:image:width", content: "1472"},
-      {property: "og:image:height", content:"832" },
-      { property: 'og:type', content: 'website' },
-      { property: 'og:updated_time', content: '1440432930' },
-    ],
-  },
+  meta: [
+    { property: 'og:site_name', content: 'Periferia digital' },
+    { property: 'og:title', content: 'Periferia Digital' },
+    { property: 'og:description', content: 'Una perspectiva digital para escalar tu negocio!' },
+    { property: 'og:image', itemprop: 'image', content: 'https://github.com/LeonAntGon/PERIFERIA-DIGITAL/blob/main/src/app/opengraph-image.jpg' },
+    {property: "og:image:width", content: "1472"},
+    {property: "og:image:height", content:"832" },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:updated_time', content: '1440432930' },
+  ],
 };
 
 export default function RootLayout({ children }) {
