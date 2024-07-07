c24e5ab894214db03e96f28807bb81f6bcd5d8a0
unknown <gonzalezleonardoantonio056@gmail.com>
2024-07-07 18:03:37 -0300

    chucha

--- src/app/desarrolloweb/tienda/page.js
+++ src/app/desarrolloweb/tienda/page.js
@@ -76,7 +76,7 @@ export default function Tienda(){
                 </h3>
 
                 <h3>
-                <svg width="34px" height="30px" stroke-width="1.2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M8 19C9.10457 19 10 18.1046 10 17C10 15.8954 9.10457 15 8 15C6.89543 15 6 15.8954 6 17C6 18.1046 6.89543 19 8 19Z" stroke="#000000" stroke-width="1.2" stroke-miterlimit="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18 19C19.1046 19 20 18.1046 20 17C20 15.8954 19.1046 15 18 15C16.8954 15 16 15.8954 16 17C16 18.1046 16.8954 19 18 19Z" stroke="#000000" stroke-width="1.2" stroke-miterlimit="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M10.05 17H15V6.6C15 6.26863 14.7314 6 14.4 6H1" stroke="#000000" stroke-width="1.2" stroke-linecap="round"></path><path d="M5.65 17H3.6C3.26863 17 3 16.7314 3 16.4V11.5" stroke="#000000" stroke-width="1.2" stroke-linecap="round"></path><path d="M2 9L6 9" stroke="#000000" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M15 9H20.6101C20.8472 9 21.0621 9.13964 21.1584 9.35632L22.9483 13.3836C22.9824 13.4604 23 13.5434 23 13.6273V16.4C23 16.7314 22.7314 17 22.4 17H20.5" stroke="#000000" stroke-width="1.2" stroke-linecap="round"></path><path d="M15 17H16" stroke="#000000" stroke-width="1.2" stroke-linecap="round"></path></svg>
+                <svg width="30px" stroke-width="1.2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M8 19C9.10457 19 10 18.1046 10 17C10 15.8954 9.10457 15 8 15C6.89543 15 6 15.8954 6 17C6 18.1046 6.89543 19 8 19Z" stroke="#000000" stroke-width="1.2" stroke-miterlimit="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18 19C19.1046 19 20 18.1046 20 17C20 15.8954 19.1046 15 18 15C16.8954 15 16 15.8954 16 17C16 18.1046 16.8954 19 18 19Z" stroke="#000000" stroke-width="1.2" stroke-miterlimit="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M10.05 17H15V6.6C15 6.26863 14.7314 6 14.4 6H1" stroke="#000000" stroke-width="1.2" stroke-linecap="round"></path><path d="M5.65 17H3.6C3.26863 17 3 16.7314 3 16.4V11.5" stroke="#000000" stroke-width="1.2" stroke-linecap="round"></path><path d="M2 9L6 9" stroke="#000000" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M15 9H20.6101C20.8472 9 21.0621 9.13964 21.1584 9.35632L22.9483 13.3836C22.9824 13.4604 23 13.5434 23 13.6273V16.4C23 16.7314 22.7314 17 22.4 17H20.5" stroke="#000000" stroke-width="1.2" stroke-linecap="round"></path><path d="M15 17H16" stroke="#000000" stroke-width="1.2" stroke-linecap="round"></path></svg>
                 Diferentes formas de entrega (Envío gratis, Retiro por tienda, Envío con costo)
                 </h3>
 
@@ -120,24 +120,24 @@ export default function Tienda(){
                     borderStyle: 'solid',
                     borderWidth: '0.4em',
                     boxShadow: '0.3em 0.3em black',
-                }} className="mx-auto my-auto mt-[120px] md:mt-0">
+                }} className="mx-auto md:my-auto mt-[120px] md:mt-0">
 
                     <div style={{
                         borderRadius: "0 0 25px 25px",
                     }} className="bg-blue-300 text-center w-[100px] mx-auto font-bold"> <p>Precio</p>
                     </div>
 
-                    <div className="my-6 bg-blue-100 text-center py-2">
+                    <div className="my-6 bg-[#edf5ff] text-center py-2">
                         <p className="font-bold text-[16px]">Desarrollo Web</p>
                         <p className="font-semibold text-[18px]"> $90.000</p>
                     </div>
 
-                    <div className="my-6 bg-blue-100 text-center py-2">
+                    <div className="my-6 bg-[#edf5ff] text-center py-2">
                         <p className="font-bold text-[16px]">Abono mensual</p>
                         <p className="font-semibold text-[18px]"> $10.000</p>
                     </div>
 
-                    <div className="mt-6 bg-blue-300 text-center h-[45px] rounded-b-lg">
+                    <div className="mt-6 bg-blue-200 text-center h-[45px] rounded-b-lg">
                         <p className="font-bold text-[16px] pt-3 ">Dominio por un año</p>
                     </div>
 
