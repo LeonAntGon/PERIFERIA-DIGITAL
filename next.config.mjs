fc8ce04424663460916bd4ef2b13c5c819ce6172
unknown <gonzalezleonardoantonio056@gmail.com>
2024-07-31 17:25:40 -0300

    eliminate trabajos

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-      config.module.rules.push({
-        test: /\.(mp4|webm|ogg|swf|ogv)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next/static/videos/',
-            outputPath: 'static/videos/',
-            name: '[name].[hash].[ext]',
-            esModule: false,
-          },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg|swf|ogv)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/videos/',
+          outputPath: 'static/videos/',
+          name: '[name].[hash].[ext]',
+          esModule: false,
         },
-      });
-  
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+      },
+    });
+
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: '/old-path',
+        destination: './', 
+        permanent: true,  
+      },
+    ];
+  },
+};
+
+export default nextConfig;
