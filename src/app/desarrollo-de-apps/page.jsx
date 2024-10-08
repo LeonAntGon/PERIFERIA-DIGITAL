9a6b2f0eacbb75d9e648d894d62c7f0e08cc32b5
unknown <gonzalezleonardoantonio056@gmail.com>
2024-10-08 16:00:15 -0300

    desarrollo de apps y nuevo prximamente

---
+++ src/app/desarrollo-de-apps/page.jsx
@@ -0,0 +1,10 @@
+import Proximamente from "../proximamente/page";
+
+export default function Automatizar(){
+    return(
+        <>
+        <Proximamente/>
+        </>
+    )
+}
+
--- src/app/proximamente/page.js
+++ src/app/proximamente/page.js
@@ -1,41 +1,16 @@
 "use client"
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
-import '../components/styles/proxeye.css';
-import '../components/styles/proxloading.css';
+import Hamster from '../components/animations/Hamster'
 
 export default function Proximamente(){
     return(
         <>
         <Nav/>
-        <main className='h-[100vh] flex flex-wrap content-center flex-col justify-center '>
-  <section className='flex justify-center'>
-    <div className="eye">
-      <div className="up"></div>
-      <div className="mid"></div> 
-      <div className="down"></div> 
-    </div>
-  </section>
-  <section className='flex justify-center'>
-    <div className="wrapper">
-      <span className="letter letter1">P</span>
-      <span className="letter letter2">r</span>
-      <span className="letter letter3">o</span>
-      <span className="letter letter4">x</span>
-      <span className="letter letter5">i</span>
-      <span className="letter letter6">m</span>
-      <span className="letter letter7">a</span>
-      <span className="letter letter8">m</span>
-      <span className="letter letter9">e</span>
-      <span className="letter letter10">n</span>
-      <span className="letter letter11">t</span>
-      <span className="letter letter12">e</span>
-      <span className="letter letter13">.</span>
-      <span className="letter letter14">.</span>
-      <span className="letter letter15">.</span>
-    </div>
-  </section>
-</main>
+        <main className='h-[100vh] flex flex-wrap content-center flex-col justify-center '>  
+          <div><Hamster/></div>
+          <div><p className='text-center text-gray-800 font-semibold mx-8 mt-4'>Debido a la alta demanda, actualmente no estamos brindando este servicio de manera temporal.</p></div>
+        </main>
         <Footer/>
         </>
     )
